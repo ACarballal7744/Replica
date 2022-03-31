7b13c1af8999d25f1c12ece596bb2973a070cead
ACarballal7744 <alan.carba@hotmail.com>
2022-03-31 14:22:09 -0300

    primer commit creamos clases

---
+++ src/main/java/Replica/Alan/ArchivoRestController.java
@@ -0,0 +1,29 @@
+package Replica.Alan;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ArchivoRestController {
+	
+	private String carpetaOrigen;
+	private String carpetaDestino;
+	
+	public void index() {
+		
+	}
+	
+	public void upload(MultipartFile zipfile) {
+		
+	}
+	
+
+}
